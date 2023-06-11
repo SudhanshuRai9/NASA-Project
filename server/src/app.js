fd4be33bd6e1c9880c9b4334113a1a43090cd9ea
Sudhanshu Rai <raisudhanshu69@gmail.com>
2023-06-11 14:06:26 +0530

    serving React front-end in production

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -8,7 +9,10 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
 
 module.exports = app;
