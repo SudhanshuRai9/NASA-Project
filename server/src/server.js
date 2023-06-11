5ff8809eb419789328a4ade5ddc56a4d2e2d79fd
Sudhanshu Rai <raisudhanshu69@gmail.com>
2023-06-11 13:43:59 +0530

    configured cors and /planets api with the frontend

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,19 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
 const PORT = process.env.PORT || 8000;
 
 // Creating server like this helps to not only respond to 'http' requests but also to other types of connections.
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
-});
+async function startServer() {
+    await loadPlanetsData();
+    
+    server.listen(PORT, () => {
+        console.log(`Listening on PORT ${PORT}`)
+    });
+};
+
+startServer();
