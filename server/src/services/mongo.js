b90a441f1eb3bfa1eb86aa36c93797dfece46166
Sudhanshu Rai <raisudhanshu69@gmail.com>
2023-06-24 14:15:22 +0530

    refactored configuration code

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = "mongodb+srv://Sudhanshu:Sudh!2000@cluster0.rqsaf.mongodb.net/nasa?retryWrites=true&w=majority"
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!');
